f61d04f338011f42e192ebf2892d072312dc3fc7
Fabian Lupa <f.lupa@flaiker.com>
2016-02-19 17:11:51 +0100

    Improve logging

--- src/com/flaiker/bottimus/Main.java
+++ src/com/flaiker/bottimus/Main.java
@@ -4,27 +4,53 @@ import com.flaiker.bottimus.configuration.Configuration;
 import com.flaiker.bottimus.configuration.ConfigurationBuilder;
 import com.flaiker.bottimus.listeners.CommandListener;
 import com.flaiker.bottimus.listeners.JoinMessageListener;
+import com.flaiker.bottimus.tools.ChannelLogHandler;
+import com.flaiker.bottimus.tools.LogFormatter;
+import net.dv8tion.jda.JDA;
 import net.dv8tion.jda.JDABuilder;
 
 import java.io.File;
+import java.io.PrintStream;
+import java.util.logging.*;
 
 public class Main {
+    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Starting");
+        // Set up logging
+        LogManager.getLogManager().reset();
+        Handler handler = new ConsoleHandler();
+        handler.setLevel(Level.ALL);
+        handler.setFormatter(new LogFormatter());
+        Logger.getLogger("").addHandler(handler);
+
+        // Redirect default System.out to the logger (because JDA does not use java logging...)
+        System.setOut(new PrintStream(System.out) {
+            @Override
+            public void print(String s) {
+                Logger.getLogger("System.out").info(s);
+            }
+        });
+
+        LOG.info("Starting...");
 
         try {
             // Load configuration
             new ConfigurationBuilder(new File("config.properties")).build();
 
             // Start JDA
-            new JDABuilder()
-                    .setEmail(Configuration.EMAIL)
-                    .setPassword(Configuration.PASSWORD)
-                    .addListener(new JoinMessageListener())
-                    .addListener(new CommandListener())
-                    .buildAsync();
+            JDA jda = new JDABuilder()
+                        .setEmail(Configuration.EMAIL)
+                        .setPassword(Configuration.PASSWORD)
+                        .addListener(new JoinMessageListener())
+                        .addListener(new CommandListener())
+                        .buildAsync();
+
+            // Log to the specified log channel
+            ChannelLogHandler channelLogHandler = new ChannelLogHandler(jda, new LogFormatter());
+            Logger.getLogger("").addHandler(channelLogHandler);
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.log(Level.SEVERE, "An unexpected error occurred", e);
         }
     }
 }
---
+++ src/com/flaiker/bottimus/tools/LogFormatter.java
@@ -0,0 +1,35 @@
+package com.flaiker.bottimus.tools;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+/**
+ * Format log output in one line
+ */
+public class LogFormatter extends Formatter {
+    public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+
+    @Override
+    public String format(LogRecord record) {
+        Optional<Throwable> thrown = Optional.ofNullable(record.getThrown());
+
+        return new StringBuilder()
+                .append(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date(record.getMillis())))
+                .append(" ")
+                .append(record.getLevel().getName())
+                .append(": ")
+                .append(formatMessage(record))
+                .append(LINE_SEPARATOR)
+                .append(thrown.map(t -> {
+                    StringWriter sw = new StringWriter();
+                    t.printStackTrace(new PrintWriter(sw));
+                    return sw.toString() + LINE_SEPARATOR;
+                }).orElse(""))
+                .toString();
+    }
+}
