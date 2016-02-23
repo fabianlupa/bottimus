ba5b6ba8d797b3432d9793b5fd10c351db76420f
Fabian Lupa <f.lupa@flaiker.com>
2016-02-23 21:01:22 +0100

    Improve logging and greeting logic

--- src/com/flaiker/bottimus/listeners/JoinMessageListener.java
+++ src/com/flaiker/bottimus/listeners/JoinMessageListener.java
@@ -1,35 +1,29 @@
 package com.flaiker.bottimus.listeners;
 
 import com.flaiker.bottimus.configuration.Configuration;
-import com.flaiker.bottimus.tools.ResourcePlayer;
+import com.flaiker.bottimus.services.AudioService;
 import net.dv8tion.jda.MessageBuilder;
-import net.dv8tion.jda.audio.player.Player;
 import net.dv8tion.jda.entities.TextChannel;
-import net.dv8tion.jda.entities.VoiceChannel;
 import net.dv8tion.jda.events.ReadyEvent;
 import net.dv8tion.jda.events.voice.VoiceJoinEvent;
 import net.dv8tion.jda.hooks.ListenerAdapter;
 
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.IOException;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 /**
  * Listener to send a greeting on startup
  */
 public class JoinMessageListener extends ListenerAdapter {
+    private static final Logger LOG = Logger.getLogger(JoinMessageListener.class.getName());
+    private final AudioService audioService;
+
+    public JoinMessageListener(AudioService audioService) {
+        this.audioService = audioService;
+    }
+
     @Override
     public void onReady(ReadyEvent event) {
-        Optional<VoiceChannel> voiceChannel =
-                event.getJDA().getVoiceChannelByName(Configuration.VOICE_CHANNEL).stream().findFirst();
-
-        if (voiceChannel.isPresent()) {
-            event.getJDA().getAudioManager().openAudioConnection(voiceChannel.get());
-            System.out.println("Joined voice channel '" + Configuration.VOICE_CHANNEL + "'");
-        } else {
-            System.out.println("Could not find voice channel '" + Configuration.VOICE_CHANNEL + "'");
-        }
-
         super.onReady(event);
     }
 
@@ -41,23 +35,12 @@ public class JoinMessageListener extends ListenerAdapter {
 
         if (channel.isPresent()) {
             channel.get().sendMessageAsync(mb.appendString(Configuration.GREETING).build(),
-                    c -> System.out.println("Send greeting message to '" + Configuration.MAIN_CHANNEL + "'"));
+                    c -> LOG.info("Send greeting message to '" + Configuration.MAIN_CHANNEL + "'"));
         } else {
-            System.out.println("Could not find channel '" + Configuration.MAIN_CHANNEL + "'");
+            LOG.warning("Could not find channel '" + Configuration.MAIN_CHANNEL + "'");
         }
 
-        try {
-            Player player = new ResourcePlayer(getClass().getResource("/hello.mp3"));
-            event.getJDA().getAudioManager().setSendingHandler(player);
-            Thread.sleep(2000);
-            player.play();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (IOException | IllegalArgumentException e) {
-            System.out.println("Could not read audio file");
-        } catch (UnsupportedAudioFileException e) {
-            System.out.println("Unsupported audio file");
-        }
+        audioService.sendGreeting();
 
         super.onVoiceJoin(event);
     }
